484493eed7408f2f409135fd61a4af4ed8542f3e
Nathalie <ngregoirecfsc@gmail.com>
2024-05-23 12:56:09 -0400

    Ajout des photos en js - Ajout des des categories en js - Page login html crée - login js en cours de finalisation

--- FrontEnd/javascript/login.js
+++ FrontEnd/javascript/login.js
@@ -1,4 +1,71 @@
-// LOGIN
+//LOGIN ADMINISTRATOR//
+
+const loginStatus = document.getElementById("login") // obtient une référence de l'élément de la page ayant l'identifiant "login"
+const logoutStatus = document.getElementById("logout")
+const adminStatus = document.getElementById("admin-logged")
+const figureModify = document.getElementById("figure-modify")
+const description = document.getElementById("figure-modify-a")
+const portfolioModify = document.getElementById("portfolio-l-modify")
+const filtreModify = document.querySelector('.filtre')
+
+//*FOOTER
+// Sélection des éléments de pied de page
+var footer = document.querySelector('footer');
+var listItem = footer.querySelector('ul li');
+// Définition `font-size` 
+listItem.style.fontSize = '14px';
+
+//* SELECTION DES LIENS DU HEADER  (MODIFICATION ESPACE ET FONT) //
+const links = document.querySelectorAll("header ul a");
+links.forEach(link => {
+    link.setAttribute("style", "margin-right: 44px; font-size: 16px; text-decoration: none;");
+});
+
+const elements = document.querySelectorAll('.connexionForm, .submitBtn');
+elements.forEach(el => el.setAttribute("style", "font-size: 14px;"));
+
+//* CONNEXION //
+const connexionSection = document.getElementById("connexion");
+connexionSection.setAttribute("style", "display: flex; flex-direction: column; align-items: center; margin: 0 auto;");
+
+const userInfo = {
+    email: document.getElementById("email"),
+    password: document.getElementById("password"),
+    connexion: document.getElementById("submitUserInfo")
+};
+Object.values(userInfo).forEach(element => {
+    element.setAttribute("style", "display: flex; flex-direction: column; align-items: flex-start;");
+});
+
+
+//* AFICHAGE DES ELEMENTS ADMINISTRATEUR //
+if (JSON.parse(sessionStorage.getItem("isConnected"))) {//Cette condition vérifie si l'utilisateur est connecté
+    loginStatus.style.display = 'none'
+    logoutStatus.style.display = 'block'
+    adminStatus.style.display = 'flex'
+    figureModify.style.display = 'flex'
+    portfolioModify.style.display = 'flex'
+    filtreModify.style.display = 'none'
+    description.style.display = 'flex' 
+} else {
+    loginStatus.style.display = 'block'
+    logoutStatus.style.display = 'none'
+    adminStatus.style.display = 'none'
+    figureModify.style.display = 'none'
+    portfolioModify.style.display = 'none'
+    filtreModify.style.display = 'flex'
+    description.style.display = 'none'
+}
+
+//*REINITIALISE L'ETAT DE CONNEXION DE L'UTILISATEUR//
+logoutStatus.addEventListener("click", (event) => {//écouteur d'événements au bouton de déconnexion
+    event.preventDefault();//empêche le comportement par défaut de l'événement
+    sessionStorage.removeItem("Token");
+    sessionStorage.removeItem("isConnected");
+    window.location.replace("index.html");// redirection vers la page "index.html"
+});
+
+//* LOGIN //
 
 const element = {
     password: document.querySelector("#password"),
--- FrontEnd/javascript/script.js
+++ FrontEnd/javascript/script.js
@@ -1,48 +1,60 @@
+const gallery = document.querySelector(".gallery")
+const categoriesContainer = document.querySelector(".categories")
 
 let allWorks =[]// initialisation du tableau
 
-const getWorks = () => {
-    try{
-
-    }catch(error)
+const getWorks = async () => {
+    try{//ouvre un bloc try qui permet de gérer une exception
+        const response = await fetch('http://localhost:5678/api/works') //envoyer une requête pour récupérer les données de l'API
+        const works = await response.json();
+        console.log("Tous les works sont",works);
+        allWorks = works;
+        for(let work of allWorks){
+            const figure = createWorkFigure(work);
+            gallery.appendChild(figure);
+        }
+    }catch(error){//Si une erreur se produit, elle est enregistrée dans la console
+        console.log('Erreur lors de la récupération des données :', error);  
+    }
 }
+getWorks()
 
-
-
-//LOGIN ADMINISTRATOR//
-
-const loginStatus = document.getElementById("login")
-const logoutStatus = document.getElementById("logout")
-const adminStatus = document.getElementById("admin-logged")
-const figureModify = document.getElementById("figure-modify")
-const description = document.getElementById("figure-modify-a")
-const portfolioModify = document.getElementById("portfolio-l-modify")
-const filtreModify = document.querySelector('.filtre')
-
-//displays the administrator elements//
-
-if (JSON.parse(sessionStorage.getItem("isConnected"))) {
-    loginStatus.style.display = 'none'
-    logoutStatus.style.display = 'block'
-    adminStatus.style.display = 'flex'
-    figureModify.style.display = 'flex'
-    portfolioModify.style.display = 'flex'
-    filtreModify.style.display = 'none'
-    description.style.display = 'flex' 
-} else {
-    loginStatus.style.display = 'block'
-    logoutStatus.style.display = 'none'
-    adminStatus.style.display = 'none'
-    figureModify.style.display = 'none'
-    portfolioModify.style.display = 'none'
-    filtreModify.style.display = 'flex'
-    description.style.display = 'none'
+const createWorkFigure = (work) => {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    img.src = work.imageUrl;
+    img.alt = work.title;
+    figure.appendChild(img);
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = work.title;
+    figure.appendChild(figcaption);
+    return figure;
 }
+// Accent grave : `
+// On récupère toutes les categories de l'API
+const API = 'http://localhost:5678/api/categories'
+const getCategories = async () => {
+    try {
+        const result = await fetch(`${API}`);
+        const data =await result.json();
+        data.unshift({//ajout de la categorie "Tous"
+            id:0,
+            name:"Tous"
+        })
+        console.log(data);
+        for(let category of data){
+            const button = document.createElement("button");
+            button.innerHTML = category.name;
+            button.setAttribute("categoryId",category.id);
+            categoriesContainer.appendChild(button);
+        }
+       
+    }catch (error) {
+        console.error (error);
+    }
+};
+getCategories()
 
-//Reset user's connexion state//
-logoutStatus.addEventListener("click", (event) => {
-    event.preventDefault();
-    sessionStorage.removeItem("Token");
-    sessionStorage.removeItem("isConnected");
-    window.location.replace("index.html");
-});
+const filterWorksByCategory = () => {
+    
+}
